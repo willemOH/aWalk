4b68a4d7a69b0fcc5bbe8f29de1453e9ac9e93dc
willemOH <49960769+willemOH@users.noreply.github.com>
2020-05-12 23:50:10 -0700

    Butterfly singularity achieved!
    that was easy! curvegen might have gotten a little wonky so have to check that next time with tester scripts (no not unit tests)

--- A Walk/Assets/Scripts/Butter_Scripts/ButterBrain.cs
+++ A Walk/Assets/Scripts/Butter_Scripts/ButterBrain.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class ButterBrain : MonoBehaviour
 {
+    public bool fly = false;
+    public bool land = false;
+    public bool idle = false;
+    public bool bezGUI = false;
+
+    public tController tcon;
+
+    public float flyspeed = 1;
+    private float current_wingspeed = 1;
+    public float wingSpeed;
+    
+    public float wingSpeedToDistanceMultiplier = 1.2f;
+    public float currentSetSpeed;
+    public float currentSpeed;
+    public float wingSpeedLive;
+    public float lerpTune = .5f;
+    [Range(.1f, 1f)]
+    public float sloMo = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,30 @@ public class ButterBrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (fly == true)
+        {
+            idle = false;
+            current_wingspeed = wingSpeed;
+            tcon.butterState = tController.state.flying;
+
+            GetComponentInChildren<tController>().globalSpeed = flyspeed * (1 - sloMo);
+            GetComponentInChildren<tController>().lerpTune = lerpTune;
+            //butterList[0].GetComponentInChildren<Animator>().speed = wingspeed / butterList[0].GetComponent<tController>().current_speed * wingSpeedToDistanceMultiplier; //changing animator speed every frame is causing wing freeze - update every other frame etc?
+            GetComponentInChildren<Animator>().speed = current_wingspeed * (1 - sloMo); //what previous line was when it worked
+            currentSetSpeed = GetComponentInChildren<tController>().speed;
+            currentSpeed = GetComponentInChildren<tController>().current_speed;
+            wingSpeedLive = GetComponentInChildren<Animator>().speed;
+        }
+        if (land == true)
+        {
+            tcon.butterState = tController.state.landing;
+            fly = false;
+            if (idle == true)
+            {
+                current_wingspeed = 1;
+                tcon.butterState = tController.state.idle;
+                land = false;
+            }
+        }
     }
 }
--- A Walk/Assets/Scripts/Butter_Scripts/ButterFlySpawner.cs
+++ A Walk/Assets/Scripts/Butter_Scripts/ButterFlySpawner.cs
@@ -8,23 +8,13 @@ public class ButterFlySpawner : MonoBehaviour
     public GameObject BezierGenPrefab;
     public RuntimeAnimatorController animator;
     public bool spawnButter = false;
-    public bool fly = false;
-    public bool land = false;
-    public bool idle = false;
-    public bool bezGUI = false;
     List<GameObject> butterList = new List<GameObject>();
 
-    private tController tcon;
-    public float flyspeed = 1;
-    public float wingspeed = 1;
+    
+
     private float selectedWingSpeed = 5;
-    public float wingSpeedToDistanceMultiplier = 1.2f;
-    public float currentSetSpeed;
-    public float currentSpeed;
-    public float wingSpeedLive;
-    public float lerpTune =.5f;
-    [Range (.1f,1f)]
-    public float sloMo = 0;
+    public float wingspeed = 1;
+    public float flyspeed = 1;
 
     void Update()
     {
@@ -36,52 +26,28 @@ public class ButterFlySpawner : MonoBehaviour
         }
         if (butterList.Count >0)
         {
-            butterList[0].GetComponentInChildren<tController>().globalSpeed = flyspeed * (1 - sloMo);
-            butterList[0].GetComponentInChildren<tController>().lerpTune = lerpTune;
-            //butterList[0].GetComponentInChildren<Animator>().speed = wingspeed / butterList[0].GetComponent<tController>().current_speed * wingSpeedToDistanceMultiplier; //changing animator speed every frame is causing wing freeze - update every other frame etc?
-            butterList[0].GetComponentInChildren<Animator>().speed = wingspeed *(1 - sloMo); //what previous line was when it worked
-            currentSetSpeed = butterList[0].GetComponentInChildren<tController>().speed;
-            currentSpeed = butterList[0].GetComponentInChildren<tController>().current_speed;
-            wingSpeedLive = butterList[0].GetComponentInChildren<Animator>().speed;
-
+           
 
         }
-        if (fly == true)
-        {
-            idle = false;
-            wingspeed = selectedWingSpeed;
-            tcon.butterState = tController.state.flying;
-        }
-        if (land == true)
-        {
-            tcon.butterState = tController.state.landing;
-            fly = false;
-            if(idle == true)
-            {
-                wingspeed = 1;
-                tcon.butterState = tController.state.idle;
-                land = false;
-            }
-        }
+       
     }
 
     IEnumerator NewButter()
     {
         GameObject butterInst = Instantiate(ButterModel);
+        ButterBrain brain = butterInst.AddComponent<ButterBrain>();
         GameObject butterMesh = butterInst.transform.GetChild(0).gameObject; //getting child mesh and adding animator
         Animator butterAnim = butterMesh.AddComponent<Animator>();           //
         butterAnim.runtimeAnimatorController = animator;                     //
         GameObject bezierFabInst = Instantiate(BezierGenPrefab);
-        tcon = bezierFabInst.GetComponent<tController>();
-        tcon.followPath = butterInst;
+        brain.tcon = bezierFabInst.GetComponent<tController>();
+        brain.tcon.followPath = butterInst;
         bezierFabInst.transform.parent = butterInst.transform;
         butterInst.transform.parent = this.transform;
-        //butterInst.transform.parent = bezierFabInst.transform;
-        //bezierFabInst.transform.parent = this.transform;
-        butterList.Add(butterInst); //question for justin - better to have butterfly in bezierfab or bezierfab in butterfly for adding to list?
-        selectedWingSpeed = wingspeed;
-        wingspeed = 1;
-        tcon.butterState = tController.state.idle;
+        butterList.Add(butterInst);
+        brain.wingSpeed = wingspeed;
+        brain.flyspeed = flyspeed;
+        brain.tcon.butterState = tController.state.idle;
 
         yield return null;
 
